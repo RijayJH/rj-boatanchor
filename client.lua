e3bed910339a18c8e3dd1d9390e0fa4a0540de3f
eXcaliber123 <108556359+eXcaliber123@users.noreply.github.com>
2023-02-25 23:14:45 -0600

    first commit

---
+++ client.lua
@@ -0,0 +1,64 @@
+local anchored = false
+RegisterNetEvent("BoatAnchor", function(boat)
+	Wait(0)
+	local ped = GetPlayerPed(-1)
+	if not IsVehicleEngineOn(boat) then
+		if not IsPedInAnyVehicle(ped) and boat ~= nil then
+			if not anchored then
+				SetBoatAnchor(boat, true)
+				TaskStartScenarioInPlace(ped, 'CODE_HUMAN_MEDIC_TEND_TO_DEAD', 0, true)
+				Citizen.Wait(10000)
+				ClearPedTasks(ped)
+				lib.notify({
+					title = 'Anchored',
+					description = 'Boat Anchored',
+					type = 'success'
+				})
+			else
+				TaskStartScenarioInPlace(ped, 'CODE_HUMAN_MEDIC_TEND_TO_DEAD', 0, true)
+				Citizen.Wait(10000)
+				SetBoatAnchor(boat, false)
+				ClearPedTasks(ped)
+				lib.notify({
+					title = 'Removed Anchor',
+					description = 'Removed Boat Anchor',
+					type = 'success'
+				})
+			end
+			anchored = not anchored
+		end
+	else
+		lib.notify({
+			title = 'Turn Boat off',
+			description = 'Make sure you turn off your engines to anchor this boat',
+			type = 'error'
+		})
+		anchored = false
+	end
+end)
+
+local isBoat = false
+RegisterNetEvent('radial:boatanchor', function()
+  local coords = GetEntityCoords(PlayerPedId())
+  local boating = lib.getClosestVehicle(coords, 3, false)
+  if GetVehicleClass(boating) and GetVehicleClass(boating) == 14 then
+    if isBoat == false then
+      lib.addRadialItem({
+        {
+            id = 'boat_anchor',
+            label = 'Boat Anchor',
+            icon = 'anchor',
+            onSelect = function()
+                TriggerEvent('BoatAnchor', boating)
+            end
+        },
+      })
+      isBoat = true
+    end
+  else
+    if isBoat == true then
+      lib.removeRadialItem('boat_anchor')
+      isBoat = false
+    end
+  end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game {'gta5'}
+lua54 'yes'
+
+author 'RijayJH'
+description 'Boat Anchor Script'
+
+client_scripts {
+    'client.lua',
+    '@ox_lib/init.lua'
+}
